13cf21d6bfebadf7db26ae637eb3231c486d0745
Ashwin James <Ashwin@scioi244-45.scioi.tu-berlin.de>
2025-12-07 10:43:20 +0100

    latest

--- bandit_train_batch.py
+++ bandit_train_batch.py
@@ -302,7 +302,7 @@ class RolloutWorker:
             session_N=session_N,
             trial_ms=3000,
             randomize_sides=True,
-            shuffle=False,
+            shuffle=True,
         )
 
     def run_session(self, agent_state_dict, probs_this_session, print_this_session=False):
