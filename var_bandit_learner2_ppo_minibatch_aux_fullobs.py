cf9f7c9a5ba29fe48673c9f892cfe5535378793a
Ashwin James <Ashwin@scioi244-45.scioi.tu-berlin.de>
2026-03-09 23:11:01 +0100

    test:removing ppo update for gate

--- var_bandit_learner2_ppo_minibatch_aux_fullobs.py
+++ var_bandit_learner2_ppo_minibatch_aux_fullobs.py
@@ -811,12 +811,12 @@ class BanditLearner(nn.Module):
                 ctx_right_obs.detach(),
                 left_feats.detach(),
                 right_feats.detach(),
-                p_self_pg,
-                p_teacher_ao_pg,
-                p_teacher_ol_pg,
-                p_belief_pg,
-                g_ao,   # live
-                g_ol,   # live
+                p_self_pg.detach(),              # do not detach for ppo
+                p_teacher_ao_pg.detach(),
+                p_teacher_ol_pg.detach(),
+                p_belief_pg.detach(),   # do not detach for ppo
+                g_ao.detach(),   # live
+                g_ol.detach(),   # live
                 use_ao.detach(),
                 use_ol.detach(),
             )
