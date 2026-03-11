c8cc985bcab6e9ec46dad1490ffd9bd8c6c59830
Ashwin James <Ashwin@scioi244-45.scioi.tu-berlin.de>
2026-03-11 16:49:35 +0100

    ablation_2

--- bandit_train_batch_evalgrid_ppo_gridK_aux_postgrid_v4_gtrace_npz.py
+++ bandit_train_batch_evalgrid_ppo_gridK_aux_postgrid_v4_gtrace_npz.py
@@ -499,10 +499,10 @@ def meta_ep_rollout(env, agent, device, session_K: int, session_N: int, worker_i
             use_ol = zeros_gate
 
             # #ablation test, set g_ao to 0.0
-            # g_ao = torch.full_like(
-            #     torch.sigmoid(agent.gate_ao(torch.cat([ctx_l_cur, ctx_r_cur, ctx_l_obs, ctx_r_obs], dim=-1))),
-            #     0.0
-            # )
+            g_ao = torch.full_like(
+                torch.sigmoid(agent.gate_ao(torch.cat([ctx_l_cur, ctx_r_cur, ctx_l_obs, ctx_r_obs], dim=-1))),
+                0.0
+            )
 
         elif curr_cond == "OL-s":
             ctx_l_cur = _ctx_for(ql, {"OL-s"})
@@ -525,10 +525,10 @@ def meta_ep_rollout(env, agent, device, session_K: int, session_N: int, worker_i
             use_ol = torch.full_like(g_ol, 1.0 if any(c == "OL-o" for c in cond_tokens) else 0.0)
 
             #ablation test, set g_ol to 0.1 (for ablation_1) or 0.0 (for ablation_2)
-            # g_ol = torch.full_like(
-            #     torch.sigmoid(agent.gate_ol(torch.cat([ctx_l_cur, ctx_r_cur, ctx_l_obs, ctx_r_obs], dim=-1))),
-            #     0.0
-            # )
+            g_ol = torch.full_like(
+                torch.sigmoid(agent.gate_ol(torch.cat([ctx_l_cur, ctx_r_cur, ctx_l_obs, ctx_r_obs], dim=-1))),
+                0.0
+            )
 
         elif curr_cond == "AO-o":
             ctx_l_cur = zeros_ctx_l
