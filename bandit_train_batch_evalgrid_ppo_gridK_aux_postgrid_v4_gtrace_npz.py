f24fd392be47f19383677c5703287a85c6075834
Ashwin James <Ashwin@scioi244-45.scioi.tu-berlin.de>
2026-03-11 09:47:36 +0100

    ablation2

--- bandit_train_batch_evalgrid_ppo_gridK_aux_postgrid_v4_gtrace_npz.py
+++ bandit_train_batch_evalgrid_ppo_gridK_aux_postgrid_v4_gtrace_npz.py
@@ -498,6 +498,12 @@ def meta_ep_rollout(env, agent, device, session_K: int, session_N: int, worker_i
             use_ao = torch.full_like(g_ao, 1.0 if any(c == "AO-o" for c in cond_tokens) else 0.0)
             use_ol = zeros_gate
 
+            #ablation test, set g_ol to 0.1
+            g_ao = torch.full_like(
+                torch.sigmoid(agent.gate_ao(torch.cat([ctx_l_cur, ctx_r_cur, ctx_l_obs, ctx_r_obs], dim=-1))),
+                0.0
+            )
+
         elif curr_cond == "OL-s":
             ctx_l_cur = _ctx_for(ql, {"OL-s"})
             ctx_r_cur = _ctx_for(qr, {"OL-s"})
@@ -521,7 +527,7 @@ def meta_ep_rollout(env, agent, device, session_K: int, session_N: int, worker_i
             #ablation test, set g_ol to 0.1
             g_ol = torch.full_like(
                 torch.sigmoid(agent.gate_ol(torch.cat([ctx_l_cur, ctx_r_cur, ctx_l_obs, ctx_r_obs], dim=-1))),
-                0.1
+                0.0
             )
 
         elif curr_cond == "AO-o":
